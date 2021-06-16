054a4c21ba6ff5453ba2748a059b8c890c319002
Mo Pishdar <SOME_RANDOM_ALIAS@example.com>
2021-06-16 19:46:48 +1000

    checks whether the website is on

--- src/SeleniumSampleTests/Tests.java
+++ src/SeleniumSampleTests/Tests.java
@@ -1,7 +1,10 @@
 package SeleniumSampleTests;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class Tests {
     public static void main(String[] args)
@@ -9,8 +12,15 @@ public class Tests {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\mohpi\\OneDrive\\Desktop\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("http://book.theautomatedtester.co.uk/");
+
+
+        //check that the website is up and running by searching for a specific text
+        WebElement header= driver.findElement(By.cssSelector("body:nth-child(2) > div.mainheading"));
+        String headerText = header.getText();
+        String expectedHeaderText = "Selenium: Beginners Guide";
+        Assert.assertEquals(headerText, expectedHeaderText);
+
         driver.quit();
-        //added just to test github
 
     }
 }
