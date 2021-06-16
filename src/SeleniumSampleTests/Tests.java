ef4fe45b3902d7071751b3d4cc4a3da09a0afafa
Mo Pishdar <SOME_RANDOM_ALIAS@example.com>
2021-06-16 19:14:23 +1000

    Initial commit

---
+++ src/SeleniumSampleTests/Tests.java
@@ -0,0 +1,15 @@
+package SeleniumSampleTests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Tests {
+    public static void main(String[] args)
+    {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\mohpi\\OneDrive\\Desktop\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://book.theautomatedtester.co.uk/");
+        driver.quit();
+
+    }
+}
